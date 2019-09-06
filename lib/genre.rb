da5ba5695deb8e8264db52d123772465513c8fe1
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-06 21:00:39 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,5 +1,21 @@
 class Genre
   attr_accessor :name
   
+  @@all = []
+  
   def intialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist
+    Song,
+  end
+  
+  
 end
