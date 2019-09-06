47b1d223c01a02172cb9b876d3e6697ba76a2933
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-06 17:36:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,26 @@
 class Artist
  
+  attr_accessor :name
   
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Songs.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def new_song(name, genre)
+    Song.new(name,self,genre)
+    
+  end
 end
